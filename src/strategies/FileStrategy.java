cbd444e447dfeb4d546d6a314566d0169cc4fa40
Paweł Recław <pawel.reclaw@gmail.com>
2017-11-29 16:57:39 +0000

    FileStrategy.java edited online with Bitbucket

--- src/strategies/FileStrategy.java
+++ src/strategies/FileStrategy.java
@@ -8,28 +8,19 @@ import java.util.Scanner;
  * Created by amen on 8/17/17.
  */
 public class FileStrategy implements IInputStrategy {
-    Scanner sc;
 
     public FileStrategy() {
-        try {
-            sc = new Scanner(new File("input.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public int getInt() {
-        return sc.nextInt();
     }
 
     @Override
     public String getString() {
-        return sc.next();
     }
 
     @Override
     public double getDouble() {
-        return sc.nextDouble();
     }
 }
