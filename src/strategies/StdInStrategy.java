56aa96ff3e3c64a2625e515c257ca9be6f0e7c32
Paweł Recław <pawel.reclaw@gmail.com>
2017-11-29 16:59:13 +0000

    StdInStrategy.java edited online with Bitbucket

--- src/strategies/StdInStrategy.java
+++ src/strategies/StdInStrategy.java
@@ -8,24 +8,19 @@ import java.util.Scanner;
  * Created by amen on 8/17/17.
  */
 public class StdInStrategy implements IInputStrategy {
-    Scanner sc;
 
     public StdInStrategy() {
-        sc = new Scanner(System.in);
     }
 
     @Override
     public int getInt() {
-        return sc.nextInt();
     }
 
     @Override
     public String getString() {
-        return sc.next();
     }
 
     @Override
     public double getDouble() {
-        return sc.nextDouble();
     }
 }
