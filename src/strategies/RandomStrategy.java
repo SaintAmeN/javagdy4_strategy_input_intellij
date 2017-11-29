e4f55e4acb5d9150c9501f77136b9d8f55a324f4
Paweł Recław <pawel.reclaw@gmail.com>
2017-11-29 16:58:44 +0000

    RandomStrategy.java edited online with Bitbucket

--- src/strategies/RandomStrategy.java
+++ src/strategies/RandomStrategy.java
@@ -8,23 +8,13 @@ import java.util.Random;
 public class RandomStrategy implements IInputStrategy {
     @Override
     public int getInt() {
-        return new Random().nextInt();
     }
 
     @Override
     public String getString() {
-        Random r = new Random();
-        int dlugosc = r.nextInt(20);
-        StringBuilder b = new StringBuilder();
-
-        for (int i = 0; i < dlugosc; i++) {
-            b.append((char)(r.nextInt(32) + 97));
-        }
-        return b.toString();
     }
 
     @Override
     public double getDouble() {
-        return new Random().nextDouble();
     }
 }
